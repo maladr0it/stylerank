f8aefc469ee60c94491869f9b09ba27b41cdfe0f
k-hoang <katherine.hoang@ymail.com>
2020-08-06 12:26:39 +0900

    #6 change difficulty ranking, implement public folder, add cover image for first challenge

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -56,6 +57,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+
+    new CopyPlugin({
+      patterns: [{ from: "public", to: path.join(__dirname, "/dist") }],
     }),
   ],
 };
